a0fb61cd8875502dc7c2d87456f187c1df5d2b82
Ivan Semenov <DevSemTsk@gmail.com>
2023-05-11 19:25:22 +0700

    updated console

--- BlockInterpreter/Scenes/Console/View/ConsoleViewController.swift
+++ BlockInterpreter/Scenes/Console/View/ConsoleViewController.swift
@@ -7,6 +7,8 @@ import UIKit
 
 final class ConsoleViewController: UIViewController {
     
+    private let outputLabel = UILabel()
+    
     private let viewModel: ConsoleViewModelType
     
     init(with viewModel: ConsoleViewModelType) {
@@ -25,11 +27,38 @@ final class ConsoleViewController: UIViewController {
     }
 
     private func setup() {
+        setupOutputLabel()
         setupSuperView()
+        setupNavigationBar()
+        setupStopBarButton()
     }
     
     private func setupSuperView() {
-        view.backgroundColor = .systemGreen
+        view.backgroundColor = .appBackground
+    }
+    
+    private func setupOutputLabel() {
+        view.addSubview(outputLabel)
+        
+        outputLabel.text = "rfrfnfnfnfnfnfnfnnfnfnfnnfnfnf"
+        outputLabel.textColor = .appWhite
+        
+        outputLabel.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(10)
+        }
+    }
+    
+    private func setupNavigationBar() {
+        navigationController?.navigationBar.titleTextAttributes = [
+            .foregroundColor: UIColor.appMain ?? .white,
+            .font: UIFont.systemFont(ofSize: 19, weight: .bold)
+        ]
+    }
+    
+    private func setupStopBarButton() {
+        let stopBarButton = UIBarButtonItem(image: UIImage(systemName: "square.fill"), style: .plain, target: self, action: nil)
+        stopBarButton.tintColor = .red
+        navigationItem.rightBarButtonItem = stopBarButton
     }
 
 }
